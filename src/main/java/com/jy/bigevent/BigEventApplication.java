3859e95c556ef1f24342a349a0dcb00ac10f86c0
yongjannes <2380444947@qq.com>
2025-03-23 16:47:48 +0800

    springboot init

---
+++ src/main/java/com/jy/bigevent/BigEventApplication.java
@@ -0,0 +1,13 @@
+package com.jy.bigevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigEventApplication.class, args);
+    }
+
+}
